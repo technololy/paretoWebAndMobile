46228bf22ad92eeeea0d33e726e346e8e97f407e
ololade ahmed oyebanji <loladeking@hotmail.com>
2021-01-16 10:29:37 +0100

    * .gitignore: first
    * Pareto.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * AppShell.xaml:
    * Pareto.csproj:
    * Info.plist:
    * Item.cs:
    * AssemblyInfo.cs:
    * AppShell.xaml.cs:
    * AppDelegate.cs:
    * GettingStarted.txt:
    * LoginPage.xaml:
    * ItemsPage.xaml:
    * AboutPage.xaml:
    * Pareto.iOS.csproj:
    * IDataStore.cs:
    * NewItemPage.xaml:
    * Entitlements.plist:
    * AboutPage.xaml.cs:
    * ItemsPage.xaml.cs:
    * LoginPage.xaml.cs:
    * MainActivity.cs:
    * ItemDetailPage.xaml:
    * MockDataStore.cs:
    * Default.png:
    * NewItemPage.xaml.cs:
    * BaseViewModel.cs:
    * icon_feed.png:
    * ItemDetailPage.xaml.cs:
    * LoginViewModel.cs:
    * ItemsViewModel.cs:
    * AboutViewModel.cs:
    * icon_about.png:
    * Default@2x.png:
    * Pareto.Android.csproj:
    * icon_feed@3x.png:
    * NewItemViewModel.cs:
    * xamarin_logo.png:
    * icon_feed@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * icon_about@3x.png:
    * icon_about@2x.png:
    * xamarin_logo@2x.png:
    * ItemDetailViewModel.cs:
    * Default-568h@2x.png:
    * xamarin_logo@3x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * Tabbar.xml:
    * styles.xml:
    * colors.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * icon.png:
    * Resource.designer.cs:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon_feed.png:
    * icon.png:
    * icon_about.png:
    * icon.png:
    * xamarin_logo.png:
    * icon.xml:
    * Icon20.png:
    * Icon29.png:
    * Icon40.png:
    * Icon58.png:
    * Icon87.png:
    * Icon80.png:
    * Icon76.png:
    * Icon60.png:
    * icon_round.xml:
    * Icon180.png:
    * Icon167.png:
    * Icon152.png:
    * Icon120.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ Pareto/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Pareto.ViewModels;
+
+namespace Pareto.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Pareto/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Pareto.Models;
+using Pareto.ViewModels;
+
+namespace Pareto.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
